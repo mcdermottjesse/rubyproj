d1b04f3e7ad2839ff4091a104be799c5ca3fbd75
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-05-28 15:36:41 -0700

    add more search capabilities

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
 
   def self.search(search)
     if search
-      where('brand LIKE :search OR model LIKE :search', search: "%#{search}%")
+      where('brand LIKE :search OR model LIKE :search OR colour LIKE :search OR cast(year as text) LIKE :search OR cast(price as text) LIKE :search', search: "%#{search}%")
     
     else
       Car.all
